a1df6039c3119b363631b901c981a35a482b5276
deepCfish <deepcfish.camellia@gmail.com>
2025-04-20 19:34:31 +0800

    debug

--- optimize1.cpp
+++ optimize1.cpp
@@ -2,14 +2,38 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#define MIN(a,b)  ((a)<(b)?(a):(b))
+
 #define LL long long
+
+void simple_sieve(LL limit, LL **primes, LL *count) {
+    char *is_prime = malloc((limit + 1) * sizeof(char));
+    for (LL i = 0; i <= limit; i++) is_prime[i] = 1;
+    is_prime[0] = is_prime[1] = 0;
+
+    for (LL i = 2; i * i <= limit; i++) {
+        if (is_prime[i]) {
+            for (LL j = i * i; j <= limit; j += i)
+                is_prime[j] = 0;
+        }
+    }
+
+    *count = 0;
+    for (LL i = 2; i <= limit; i++)
+        if (is_prime[i]) (*count)++;
+
+    *primes = malloc((*count) * sizeof(LL));
+    LL idx = 0;
+    for (LL i = 2; i <= limit; i++)
+        if (is_prime[i]) (*primes)[idx++] = i;
+
+    free(is_prime);
+}
 
 int main(int argc, char* argv[]) {
     LL count = 0, global_count = 0;
     double elapsed_time;
     LL low_index, high_index, size;
-    LL i, prime, index, first;
+    LL i, prime, first;
     int id, p;
     char *marked;
     LL n;
@@ -33,7 +57,7 @@ int main(int argc, char* argv[]) {
         return 0;
     }
 
-    // Only consider odd numbers: 3, 5, 7, ..., n
+    // Only odd numbers > 2 are considered
     LL odd_count = (n - 1) / 2; // exclude even numbers, index 0 maps to 3
     low_index = id * odd_count / p;
     high_index = (id + 1) * odd_count / p - 1;
@@ -47,52 +71,52 @@ int main(int argc, char* argv[]) {
     }
     for (i = 0; i < size; i++) marked[i] = 0;
 
-    if (!id) index = 0;
-    prime = 3;
+    // Only process 0 computes the small prime list
+    LL *small_primes = NULL;
+    LL prime_count = 0;
+    if (id == 0) {
+        simple_sieve((LL)sqrt(n), &small_primes, &prime_count);
+    }
 
-    do {
-        // prime index in odd space: (prime - 3)/2
-        LL prime_index = (prime - 3) / 2;
+    // Broadcast prime count and primes to all processes
+    MPI_Bcast(&prime_count, 1, MPI_LONG_LONG, 0, MPI_COMM_WORLD);
+    if (id != 0) small_primes = malloc(prime_count * sizeof(LL));
+    MPI_Bcast(small_primes, prime_count, MPI_LONG_LONG, 0, MPI_COMM_WORLD);
 
-        if (prime * prime > (2 * low_index + 3))
+    for (LL k = 0; k < prime_count; k++) {
+        prime = small_primes[k];
+
+        // Mapping from index to value: value = 2 * idx + 3
+        LL low_value = 2 * low_index + 3;
+
+        if (prime * prime > low_value) {
             first = (prime * prime - 3) / 2 - low_index;
-        else {
-            LL low_value = 2 * low_index + 3;
-            if (low_value % prime == 0)
-                first = 0;
-            else
-                first = prime - (low_value % prime);
-            if (((2 * low_index + 3 + first) % 2) == 0) first++;
-            first = (first + 1) / 2;
+        } else {
+            LL rem = low_value % prime;
+            LL delta = (rem == 0) ? 0 : prime - rem;
+            if ((low_value + delta) % 2 == 0) delta += prime;
+            first = (delta + 1) / 2;
         }
 
         for (i = first; i < size; i += prime)
             marked[i] = 1;
-
-        if (!id) {
-            while (marked[++index]);
-            prime = 2 * index + 3;
-        }
-        if (p > 1) MPI_Bcast(&prime, 1, MPI_LONG_LONG, 0, MPI_COMM_WORLD);
-
-    } while (prime * prime <= n);
+    }
 
     count = 0;
     for (i = 0; i < size; i++)
         if (!marked[i]) count++;
 
     MPI_Reduce(&count, &global_count, 1, MPI_LONG_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
-
     elapsed_time += MPI_Wtime();
 
     if (!id) {
-        // Add 1 for the prime number 2
-        global_count += 1;
+        global_count += 1; // Include prime number 2
         printf("There are %lld primes less than or equal to %lld\n", global_count, n);
         printf("Optimized SIEVE (%d processes): %10.6f seconds\n", p, elapsed_time);
     }
 
     free(marked);
+    free(small_primes);
     MPI_Finalize();
     return 0;
 }
