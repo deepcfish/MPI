e0148b4fdd750584b43b6dfd5a29d2da42e33bec
deepCfish <deepcfish.camellia@gmail.com>
2025-04-21 10:02:34 +0800

    debug

--- optimize3.cpp
+++ optimize3.cpp
@@ -3,39 +3,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MIN(a,b)  ((a)<(b)?(a):(b))
+#define MIN(a,b) ((a)<(b)?(a):(b))
 #define LL long long
 
-// Generate small odd primes up to sqrt(n)
 void generate_small_primes(char *small_marked, LL sqrt_n, LL *small_primes, LL *count) {
     for (LL i = 0; i <= sqrt_n; ++i) small_marked[i] = 0;
-    for (LL i = 3; i * i <= sqrt_n; i += 2) {
+    for (LL i = 2; i * i <= sqrt_n; ++i) {
         if (!small_marked[i]) {
-            for (LL j = i * i; j <= sqrt_n; j += 2 * i)
+            for (LL j = i * i; j <= sqrt_n; j += i)
                 small_marked[j] = 1;
         }
     }
     *count = 0;
-    small_primes[(*count)++] = 2;
-    for (LL i = 3; i <= sqrt_n; i += 2) {
+    for (LL i = 2; i <= sqrt_n; ++i) {
         if (!small_marked[i]) {
             small_primes[(*count)++] = i;
         }
     }
 }
 
-int main(int argc, char* argv[]) {
-    LL count;
-    double elapsed_time;
-    LL global_count = 0;
-    LL high_index, low_index;
-    LL i, j;
-    int id;
-    char *marked;
-    LL n;
-    int p;
-    LL size;
-    const LL BLOCK_SIZE = 32768;
+
+int main(int argc, char* argv[])
+{
+    LL count;              // Local prime count
+    double elapsed_time;   // Parallel execution time
+    LL first;              // Index of first multiple
+    LL global_count = 0;   // Global prime count
+    LL high_value;         // Highest value on this proc
+    LL i;
+    int id;                // Process ID number
+    LL low_value;          // Lowest value on this proc
+    char* marked;          // Portion of 2,...,'n'
+    LL n;                  // Sieving from 2, ..., 'n'
+    int p;                 // Number of processes
+    LL proc0_size;         // Size of proc 0's subarray
+    LL size;               // Elements in 'marked'
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &id);
@@ -44,70 +46,91 @@ int main(int argc, char* argv[]) {
     elapsed_time = -MPI_Wtime();
 
     if (argc != 2) {
-        if (!id) printf("Usage: %s <n>\n", argv[0]);
+        if (!id) printf("Command line: %s <m>\n", argv[0]);
         MPI_Finalize();
         exit(1);
     }
 
     n = atoll(argv[1]);
-    LL sqrt_n = (LL)sqrt((double)n);
 
-    // Only store odd numbers > 2: index i represents number 2*i + 3
-    LL total_odd_count = (n - 1) / 2;
-    low_index = id * total_odd_count / p;
-    high_index = ((id + 1) * total_odd_count / p) - 1;
-    size = high_index - low_index + 1;
+    low_value = 2 + id * (n - 1) / p;
+    high_value = 1 + (id + 1) * (n - 1) / p;
+    if (low_value % 2 == 0) low_value++;
+    if (high_value % 2 == 0) high_value--;
+    size = (high_value - low_value) / 2 + 1;
+    count=size;
 
+    proc0_size = (n - 1) / p;
+    if ((2 + proc0_size) < (int)sqrt((double)n)) {
+        if (!id) printf("Too many processes\n");
+        MPI_Finalize();
+        exit(1);
+    }
     marked = (char*)malloc(size);
-    for (i = 0; i < size; i++) marked[i] = 0;
+    if (marked == NULL) {
+        printf("Cannot allocate enough memory\n");
+        MPI_Finalize();
+        exit(1);
+    }
 
-    char *small_marked = (char*)malloc((sqrt_n + 1));
-    LL *small_primes = (LL*)malloc((sqrt_n + 1) * sizeof(LL));
+
+    LL *small_primes = NULL;
+    char *small_marked = NULL;
     LL small_count = 0;
-    generate_small_primes(small_marked, sqrt_n, small_primes, &small_count);
-    free(small_marked);
-
-    // Perform blocked sieve
-    for (LL block_low = 0; block_low < size; block_low += BLOCK_SIZE) {
-        LL block_high = MIN(block_low + BLOCK_SIZE, size);
-        for (j = 0; j < small_count; ++j) {
-            LL prime = small_primes[j];
-            LL prime2 = 2 * prime;
-            LL first_val = 2 * low_index + 3 + 2 * block_low;
-            LL first;
-
-            if (prime * prime > first_val)
-                first = (prime * prime - 3) / 2 - low_index;
-            else {
-                LL r = first_val % prime;
-                if (r == 0) first = block_low;
-                else {
-                    LL delta = prime - r;
-                    if ((first_val + delta) % 2 == 0) delta += prime;
-                    first = (first_val + delta - 3) / 2 - low_index;
-                }
-            }
-            for (i = first; i < block_high; i += prime2) marked[i] = 1;
+    LL sqrt_n = (LL)sqrt((double)n);
+    small_primes = (LL*)malloc((sqrt_n + 1) * sizeof(LL));
+    if (id == 0) {
+        small_marked = (char*)malloc((sqrt_n + 1) * sizeof(char));
+        generate_small_primes(small_marked, sqrt_n, small_primes, &small_count);
+        free(small_marked);
+    }else{
+        LL rem_ = low_value % 3;
+        LL first_ = (rem == 0) ? low_value : (low_value + 3 - rem);
+        for(i=first_;i<=high_value;i+=6){
+            marked[(i - low_value) / 2] = 1;
+            count--;
         }
     }
 
-    free(small_primes);
-    count = 0;
-    for (i = 0; i < size; i++)
-        if (!marked[i]) count++;
-    free(marked);
+    MPI_Bcast(&small_count, 1, MPI_LONG_LONG, 0, MPI_COMM_WORLD);
+    MPI_Bcast(small_primes, small_count, MPI_LONG_LONG, 0, MPI_COMM_WORLD);
 
-    if (2 <= n) count++; // account for prime number 2
+
+    for (i = 0; i < size; i++) marked[i] = 0;
+
+
+    for (int j =1; j < small_count; j++) {
+        LL  prime = small_primes[j];
+
+        LL first;
+        if (prime * prime > low_value)
+            first = prime * prime;
+        else {
+            LL rem = low_value % prime;
+            first = (rem == 0) ? low_value : (low_value + prime - rem);
+        }
+        if (first % 2 == 0) first += prime;
+
+        for (i = first; i <= high_value; i += 2 * prime) {
+            if(!marked[(i - low_value) / 2] ){
+                marked[(i - low_value) / 2] = 1;
+                count--;
+            }
+        }
+    }
+
+    //for (i = 0; i < size; i++)
+        //if (!marked[i]) count++;
 
     MPI_Reduce(&count, &global_count, 1, MPI_LONG_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
     elapsed_time += MPI_Wtime();
 
     if (!id) {
-        printf("There are %lld primes <= %lld\n", global_count, n);
-        printf("SIEVE (no evens, no Bcast, %d processes) %10.6f seconds\n", p, elapsed_time);
-        printf("the process id: %d \n",id);
+        //printf("%lld",small_count);
+        printf("There are %lld primes less than or equal to %lld\n", global_count+1, n);
+        printf("SIEVE (%d processes) took %10.6f seconds.\n", p, elapsed_time);
     }
-    fflush(stdout);
+
     MPI_Finalize();
     return 0;
 }
