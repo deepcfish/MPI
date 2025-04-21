20fa17c1470286c4606a82fb91ceb0709286090d
deepCfish <deepcfish.camellia@gmail.com>
2025-04-21 11:24:23 +0800

    debug

--- optimize4.cpp
+++ optimize4.cpp
@@ -107,8 +107,26 @@ int main(int argc, char* argv[])
         LL block_end = MIN(block_start + BLOCK_SIZE, size);
         
         // 初始化标记数组
-        #pragma omp simd
-        for (LL i = block_start; i < block_end; i++) {
+        LL i = block_start;
+        for (; i + 15 < block_end; i += 16) {
+            marked[i] = 0;
+            marked[i + 1] = 0;
+            marked[i + 2] = 0;
+            marked[i + 3] = 0;
+            marked[i + 4] = 0;
+            marked[i + 5] = 0;
+            marked[i + 6] = 0;
+            marked[i + 7] = 0;
+            marked[i + 8] = 0;
+            marked[i + 9] = 0;
+            marked[i + 10] = 0;
+            marked[i + 11] = 0;
+            marked[i + 12] = 0;
+            marked[i + 13] = 0;
+            marked[i + 14] = 0;
+            marked[i + 15] = 0;
+        }
+        for (; i < block_end; i++) {
             marked[i] = 0;
         }
         
